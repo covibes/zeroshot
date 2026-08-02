aadc49a707aa9a01828976361264efce0a2c49e5
Michael Eichelbeck <michael@theopenengine.com>
2026-08-02 11:01:31 +0000

    test(rust): authorize hosted OECP topology

--- zeroshot-rust/tests/architecture.rs
+++ zeroshot-rust/tests/architecture.rs
@@ -123,6 +123,7 @@ fn workspace_metadata_preserves_package_lib_and_bin_identity() {
         })
         .collect::<BTreeSet<_>>();
     for required in [
+        ("zeroshot-oecp-server".to_owned(), "bin".to_owned()),
         ("zeroshot-rust".to_owned(), "bin".to_owned()),
         ("zeroshot_engine".to_owned(), "lib".to_owned()),
         ("architecture".to_owned(), "test".to_owned()),
@@ -152,6 +153,7 @@ fn workspace_metadata_preserves_package_lib_and_bin_identity() {
             .cloned()
             .collect::<BTreeSet<_>>(),
         BTreeSet::from([
+            ("zeroshot-oecp-server".to_owned(), "bin".to_owned()),
             ("zeroshot-rust".to_owned(), "bin".to_owned()),
             ("zeroshot_engine".to_owned(), "lib".to_owned()),
         ]),
@@ -699,6 +701,7 @@ fn product_modules_require_issue_authorization() {
         BTreeSet::from([
             "artifact_store",
             "artifact_store.rs",
+            "bin",
             "cluster_ledger",
             "cluster_ledger.rs",
             "daemon_auth.rs",
@@ -709,6 +712,8 @@ fn product_modules_require_issue_authorization() {
             "fault",
             "fault.rs",
             "full_v1_reducer.rs",
+            "hosted_oecp",
+            "hosted_oecp.rs",
             "issue_provider",
             "issue_provider.rs",
             "lib.rs",
