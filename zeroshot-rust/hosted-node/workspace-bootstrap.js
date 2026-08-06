a0d159f801b66ddabd6d2890f6ac010d59b63dd2
Michael Eichelbeck <michael@theopenengine.com>
2026-08-06 08:19:59 +0000

    refactor(hosted): use system CA trust

--- zeroshot-rust/hosted-node/workspace-bootstrap.js
+++ zeroshot-rust/hosted-node/workspace-bootstrap.js
@@ -36,8 +36,6 @@ function fixedGitArguments(args) {
     '-c',
     'http.followRedirects=false',
     '-c',
-    'http.sslCAInfo=/etc/ssl/certs/ca-certificates.crt',
-    '-c',
     'http.proxy=',
     '-c',
     'https.proxy=',
